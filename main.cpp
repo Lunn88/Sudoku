6b7207296a824fa7d6bfc81b1646da3f6b710f27
hewenxuan0 <83746187+hewenxuan0@users.noreply.github.com>
2023-06-29 15:14:33 +0800

    finish -m,-r,-u

--- main.cpp
+++ main.cpp
@@ -34,16 +34,72 @@ int main(int argc, char** argv) {
 	}
 
 	else if (strcmp(argv[1], "-n") == 0) {
-		int num = atoi(argv[2]);
-		FILE* fp;
-		fp = fopen("./game.txt", "w+");
-		while (num > 0) {
-			sudoku.create();
-			sudoku.genPuzzleWithNum(30);
-			sudoku.printFormattedGrid(fp);
-			num--;
+		if (argc == 3) {
+			int num = atoi(argv[2]);
+			FILE* fp;
+			fp = fopen("./game.txt", "w+");
+			while (num > 0) {
+				sudoku.create();
+				sudoku.genPuzzleWithNum(30);
+				sudoku.printFormattedGrid(fp);
+				num--;
+			}
+			fclose(fp);
 		}
-		fclose(fp);
+		else if (argc == 5 && strcmp(argv[3], "-m") == 0) {
+			int dif = atoi(argv[4]);
+			int num = atoi(argv[2]);
+			FILE* fp;
+			fp = fopen("./game.txt", "w+");
+			while (num > 0) {
+				sudoku.create();
+				sudoku.genPuzzleWithDifficulty(dif);
+				sudoku.printFormattedGrid(fp);
+				num--;
+			}
+			fclose(fp);
+		}
+		else if (argc == 5 && strcmp(argv[3], "-r") == 0) {
+			char begin[2];
+			char end[2];
+			begin[0] = argv[4][0];
+			begin[1] = argv[4][1];
+			end[0] = argv[4][3];
+			end[1] = argv[4][4];
+			int rbegin = atoi(begin);
+			int rend = atoi(end);
+
+			int num = atoi(argv[2]);
+			FILE* fp;
+			fp = fopen("./game.txt", "w+");
+			while (num > 0) {
+				sudoku.create();
+				int r = rbegin + rand() % (rend - rbegin);
+				sudoku.genPuzzleWithNum(r);
+				sudoku.printFormattedGrid(fp);
+				num--;
+			}
+			fclose(fp);
+			
+
+		}
+		else if (argc == 4 && strcmp(argv[3], "-u") == 0) {
+			int num = atoi(argv[2]);
+			FILE* fp;
+			fp = fopen("./game.txt", "w+");
+			while (num > 0) {
+				sudoku.create();
+				sudoku.genUniquePuzzle();
+				sudoku.printFormattedGrid(fp);
+				num--;
+			}
+			fclose(fp);
+		}
+		
+	}
+	else {
+		cout << "Arguments ERROR!" << endl;
+		return 0;
 	}
 
 	return 0;
